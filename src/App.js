4e83de60569c9e022e77c7102d4522ccb0a909e8
Florent GAUDIN <florent.gaudin@etu.utc.fr>
2020-11-05 13:41:43 +0100

    [improvement] App class component to App hook component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: [
+function App() {
+    const [todos] = useState([
         { id: 1, done: false, text: 'Add new todo with form' },
         { id: 2, done: false, text: 'Create Components : TodoForm, TodoList, TodoItem ...' },
         { id: 3, done: false, text: 'Mark done an item' },
-        { id: 4, done: false, text: 'Remove an item' },
-      ],
-    }
-  }
-
-  render() {
-    const { todos } = this.state;
+        { id: 4, done: false, text: 'Remove an item' }
+    ]);
 
     return (
-      <div className="App">
-        <div className="wrapper">
-          <form>
-            <input
-              type="text"
-              placeholder="Add a new todo"
-            />
-            <button type="submit">Add</button>
-          </form>
+        <div className="App">
+            <div className="wrapper">
+                <form>
+                    <input
+                        type="text"
+                        placeholder="Add a new todo"
+                    />
+                    <button type="submit">Add</button>
+                </form>
 
-          <ul>
-            {
-              todos.map((item, index) => (
-                  <li key={item.id}>
-                    <span className={item.done ? 'done' : 'undone'}>&#9675;</span>
-                    {item.text}
-                    <span className="remove">X</span>
-                  </li>
-              ))
-            }
-          </ul>
+                <ul>
+                    {
+                        todos.map((item, index) => (
+                            <li key={item.id}>
+                                <span className={item.done ? 'done' : 'undone'}>&#9675;</span>
+                                {item.text}
+                                <span className="remove">X</span>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
         </div>
-      </div>
     );
-  }
 }
 
 export default App;
